4d2d63ddd233653c18b1040e91b0f114940942ad
Kai Schnittcher <kai.schnittcher@gmail.com>
2017-12-04 13:14:48 +0100

    Fix PowerOnState

--- IPS-TasmotaLED/module.php
+++ IPS-TasmotaLED/module.php
@@ -54,7 +54,7 @@ class IPS_TasmotaLED extends TasmotaService {
   		   $this->SendDebug("PowerOnState Topic", $Buffer->TOPIC,0);
   		   $this->SendDebug("PowerOnState MSG", $Buffer->MSG,0);
          $MSG = json_decode($Buffer->MSG);
-         if ($MSG->PowerOnState <> $this->ReadPropertyInteger) {
+         if ($MSG->PowerOnState <> $this->ReadPropertyInteger("PowerOnState")) {
            IPS_SetProperty($this->InstanceID, "PowerOnState", $MSG->PowerOnState);
            if(IPS_HasChanges($this->InstanceID))
            {
