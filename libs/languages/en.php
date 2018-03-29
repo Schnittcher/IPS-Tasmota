63d7c0e87efa509f15e99a56c20d439fb0771b3c
Kai Schnittcher <kai.schnittcher@gmail.com>
2018-03-29 10:05:59 +0000

    Apply fixes from StyleCI

--- libs/languages/en.php
+++ libs/languages/en.php
@@ -1,48 +1,47 @@
-<?
+<?php
 
-class translate {
-  const Wifi = "Wifi";
-  const RESULT = "RESULT";
-  const STATE = "STATE";
-  const UPTIME = "UPTIME";
-  const MARGINS = "MARGINS";
-  const WARNING = "WARNING";
-  const ENERGY = "ENERGY";
+class translate
+{
+    const Wifi = 'Wifi';
+    const RESULT = 'RESULT';
+    const STATE = 'STATE';
+    const UPTIME = 'UPTIME';
+    const MARGINS = 'MARGINS';
+    const WARNING = 'WARNING';
+    const ENERGY = 'ENERGY';
 
-  //Sensor
-  const Temperature = "Temperature";
-  const Humidity = "Humidity";
+    //Sensor
+    const Temperature = 'Temperature';
+    const Humidity = 'Humidity';
 
-  const SENSOR = "SENSOR";
-  const POWER = "POWER";
-  const PowerOnState = "PowerOnState";
+    const SENSOR = 'SENSOR';
+    const POWER = 'POWER';
+    const PowerOnState = 'PowerOnState';
 
-  //switch
-  const PowerTrue = "true";
-  const PowerFalse = "false";
+    //switch
+    const PowerTrue = 'true';
+    const PowerFalse = 'false';
 
-  //Sonoff POW
-  const Total = "Total";
-  const Yesterday = "Yesterday";
-  const Today = "Today";
-  const Period = "Period";
-  const Powerusage = "Power";
-  const Powerfactor = "Factor";
-  const Voltage = "Voltage";
-  const Current = "Current";
+    //Sonoff POW
+    const Total = 'Total';
+    const Yesterday = 'Yesterday';
+    const Today = 'Today';
+    const Period = 'Period';
+    const Powerusage = 'Power';
+    const Powerfactor = 'Factor';
+    const Voltage = 'Voltage';
+    const Current = 'Current';
 
-  const Color = "Color";
-  const ColorTemperature = "CT";
-  const Dimmer = "Dimmer";
-  const Led = "Led";
-  const LedTable = "LedTable";
-  const Fade = "Fade";
-  const Pixels = "Pixels";
-  const Scheme = "Scheme";
-  const Speed = "Speed";
-  const Wakeup = "Wakeup";
-  const WakeUpDuration = "WakeUpDuration";
-  const Width = "Width";
-
+    const Color = 'Color';
+    const ColorTemperature = 'CT';
+    const Dimmer = 'Dimmer';
+    const Led = 'Led';
+    const LedTable = 'LedTable';
+    const Fade = 'Fade';
+    const Pixels = 'Pixels';
+    const Scheme = 'Scheme';
+    const Speed = 'Speed';
+    const Wakeup = 'Wakeup';
+    const WakeUpDuration = 'WakeUpDuration';
+    const Width = 'Width';
 }
-?>
