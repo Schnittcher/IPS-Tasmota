688446b9b5c2076a63ed10f25f6304f4e27226aa
Kai Schnittcher <kai.schnittcher@gmail.com>
2018-07-11 21:08:05 +0200

    Switch Topic Instanz hinzugefügt

---
+++ IPS-TasmotaSwitchTopic/module.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once __DIR__ . '/../libs/TasmotaService.php';
+
+class IPS_TasmotaSwitchTopic extends TasmotaService
+{
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
+        $this->ConnectParent('{EE0D345A-CF31-428A-A613-33CE98E752DD}');
+        //Anzahl die in der Konfirgurationsform angezeigt wird - Hier Standard auf 1
+        $this->RegisterPropertyString('SwitchTopicPräfix', '');
+    }
+
+    public function ApplyChanges()
+    {
+        //Never delete this line!
+        parent::ApplyChanges();
+        $this->ConnectParent('{EE0D345A-CF31-428A-A613-33CE98E752DD}');
+        //Setze Filter für ReceiveData
+        $SwitchTopicPraefix = $this->ReadPropertyString('SwitchTopicPräfix');
+        $this->SetReceiveDataFilter('.*' . $SwitchTopicPraefix . '.*');
+    }
+
+    public function ReceiveData($JSONString)
+    {
+        if (!empty($this->ReadPropertyString('SwitchTopicPräfix'))) {
+            $this->SendDebug('ReceiveData JSON', $JSONString, 0);
+            $data = json_decode($JSONString);
+
+            // Buffer decodieren und in eine Variable schreiben
+            $Buffer = json_decode($data->Buffer);
+            $MSG = json_decode($Buffer->MSG);
+            $this->SendDebug('Topic', $Buffer->TOPIC, 0);
+            $SwitchTopic = explode("/", $Buffer->TOPIC);
+            switch ($Buffer->TOPIC) {
+            case 'cmnd/' . $SwitchTopic[1] . '/POWER1':
+                if (property_exists($MSG, 'PowerOnState')) {
+                    $this->SendDebug('Receive SwitchTopic ($SwitchTopic[1]) Result : ', $MSG, 0);
+                   $variablenID = $this->RegisterVariableBoolean("Tasmota_".$SwitchTopic[1], "SwtichTopic ".$SwitchTopic[1]);
+                   if ($MSG == "ON") {
+                       SetValue($this->GetIDForIdent("Tasmota_".$SwitchTopic[1]), true);
+                   } else {
+                       SetValue($this->GetIDForIdent("Tasmota_" . $SwitchTopic[1]), false);
+                   }
+                }
+                break;
+            }
+        }
+    }
+}
