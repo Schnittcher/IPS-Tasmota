f597f4b5dcb300805af2bf6c425a882ed709f079
Kai Schnittcher <kai.schnittcher@gmail.com>
2019-02-13 21:19:02 +0100

    Version 3.1 FanSpeed hinzugefügt

--- IPS-Tasmota/module.php
+++ IPS-Tasmota/module.php
@@ -26,6 +26,7 @@ class IPS_Tasmota extends TasmotaService
         //Settings
         $this->RegisterPropertyBoolean('SystemVariables', false);
         $this->RegisterPropertyBoolean('Power1Deactivate', false);
+        $this->RegisterPropertyBoolean('Fan', false);
         //Debug Optionen
         $this->RegisterPropertyBoolean('Sensoren', false);
         $this->RegisterPropertyBoolean('State', false);
@@ -40,6 +41,12 @@ class IPS_Tasmota extends TasmotaService
         $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
         //Setze Filter fÃ¼r ReceiveData
         $this->setPowerOnState($this->ReadPropertyInteger('PowerOnState'));
+
+        if ($this->ReadPropertyBoolean('Fan')) {
+            $this->RegisterProfileInteger('Tasmota.FanSpeed', 'Speedo', '', '', 0, 3, 1);
+            $this->RegisterVariableInteger('Tasmota_FanSpeed', 'Speed', 'Tasmota.FanSpeed', 0);
+            $this->EnableAction('Tasmota_FanSpeed');
+        }
 
         $this->SendDebug(__FUNCTION__ . ' FullTopic', $this->ReadPropertyString('FullTopic'), 0);
         $topic = $this->FilterFullTopicReceiveData();
@@ -126,7 +133,37 @@ class IPS_Tasmota extends TasmotaService
                     $this->traverseArray($myBuffer, $myBuffer);
                 }
             }
+            //FanSpeed
+            if (fnmatch('*FanSpeed*', $Buffer->Payload)) {
+                $myBuffer = json_decode($Buffer->Payload);
+                if (property_exists($myBuffer, 'FanSpeed')) {
+                    $this->SendDebug('FandSpeed',$Buffer->Payload,0);
+                    SetValue($this->GetIDForIdent('Tasmota_FanSpeed'), $myBuffer->FanSpeed);
+                }
+            }
+
             //POW Variablen
+            if (fnmatch('*ENERGY*', $Buffer->Payload)) {
+                $myBuffer = json_decode($Buffer->Payload);
+                if (property_exists($myBuffer, 'ENERGY')) {
+                    $this->Debug('ENERGY Payload', $Buffer->Payload, 'Pow');
+                    $this->RegisterVariableFloat('Tasmota_POWTotal', 'Total', '~Electricity');
+                    $this->RegisterVariableFloat('Tasmota_POWYesterday', 'Yesterday', '~Electricity');
+                    $this->RegisterVariableFloat('Tasmota_POWToday', 'Today', '~Electricity');
+                    $this->RegisterVariableFloat('Tasmota_POWPower', 'Power', '~Watt.3680');
+                    $this->RegisterVariableFloat('Tasmota_POWFactor', 'Factor');
+                    $this->RegisterVariableFloat('Tasmota_POWVoltage', 'Voltage', '~Volt');
+                    $this->RegisterVariableFloat('Tasmota_POWCurrent', 'Current', '~Ampere');
+
+                    SetValue($this->GetIDForIdent('Tasmota_POWPower'), $myBuffer->ENERGY->Power);
+                    SetValue($this->GetIDForIdent('Tasmota_POWTotal'), $myBuffer->ENERGY->Total);
+                    SetValue($this->GetIDForIdent('Tasmota_POWToday'), $myBuffer->ENERGY->Today);
+                    SetValue($this->GetIDForIdent('Tasmota_POWYesterday'), $myBuffer->ENERGY->Yesterday);
+                    SetValue($this->GetIDForIdent('Tasmota_POWCurrent'), $myBuffer->ENERGY->Current);
+                    SetValue($this->GetIDForIdent('Tasmota_POWVoltage'), $myBuffer->ENERGY->Voltage);
+                    SetValue($this->GetIDForIdent('Tasmota_POWFactor'), $myBuffer->ENERGY->Factor);
+                }
+            }
             if (fnmatch('*ENERGY*', $Buffer->Payload)) {
                 $myBuffer = json_decode($Buffer->Payload);
                 if (property_exists($myBuffer, 'ENERGY')) {
@@ -155,12 +192,26 @@ class IPS_Tasmota extends TasmotaService
     {
         $this->SendDebug(__FUNCTION__ . ' Ident', $Ident, 0);
         $this->SendDebug(__FUNCTION__ . ' Value', $Value, 0);
+        if ($Ident == 'Tasmota_FanSpeed') {
+            $result = $this->setFanSpeed($Value);
+            return true;
+        }
+
         if (strlen($Ident) != 13) {
             $power = substr($Ident, 13);
         } else {
             $power = 0;
         }
         $result = $this->setPower($power, $Value);
+    }
+
+    public function setFanSpeed(int $value)
+    {
+        $command = 'FanSpeed';
+        $msg = strval($value);
+        $DataJSON = $this->MQTTCommand($command, $msg);
+        $this->SendDebug('setFanSpeed', $DataJSON, 0);
+        $this->SendDataToParent($DataJSON);
     }
 
     private function createVariablenProfiles()
