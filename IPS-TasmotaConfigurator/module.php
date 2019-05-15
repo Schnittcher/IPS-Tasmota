895ce571654912299610f29d382672773e16ded9
Kai Schnittcher <kai.schnittcher@gmail.com>
2019-05-15 14:56:05 +0200

    Version 3.5 - Neuer Tasmota Konfigurator

--- IPS-TasmotaConfigurator/module.php
+++ IPS-TasmotaConfigurator/module.php
@@ -1,156 +1,154 @@
 <?php
-
+ini_set( 'max_execution_time', 0);
 require_once __DIR__ . '/../libs/TasmotaService.php';
 class IPS_TasmotaConfigurator extends TasmotaService
 {
-    private $DevicesTopics = array();
-
     public function Create()
     {
         //Never delete this line!
         parent::Create();
         $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
-        $this->RegisterPropertyString('FullTopic', '%prefix%/%topic%');
-        $this->DevicesTopics = array();
+        $this->RegisterPropertyString('StartIP', '');
+        $this->RegisterPropertyString('EndIP', '');
     }
 
     public function ApplyChanges()
     {
         //Apply filter
-        $this->SetReceiveDataFilter('.*LWT.*');
         parent::ApplyChanges();
-        $this->SetBuffer('DevicesTopics', serialize($this->DevicesTopics));
+    }
+
+    private function searchTasmotaDevice($topic)
+    {
+        $idsTasmota = IPS_GetInstanceListByModuleID('{1349F095-4820-4DB8-82EB-C1E93E680F08}');
+        $idsTasmotaLed = IPS_GetInstanceListByModuleID('{5466CCED-1DA1-4FD9-9CBD-18E9399EFF42}');
+        $ids = array_merge($idsTasmota,$idsTasmotaLed);
+        foreach ($ids as $id) {
+            if (IPS_GetProperty($id, 'Topic') == $topic) {
+                return $id;
+            }
+        }
+        return 0;
     }
 
     public function GetConfigurationForm()
     {
-        $DevicesTopics = unserialize($this->GetBuffer('DevicesTopics'));
-        if (count($DevicesTopics) == 0) {
-            @$this->ScanTasmotaNetwork();
-            IPS_Sleep(2000);
-            $DevicesTopics = unserialize($this->GetBuffer('DevicesTopics'));
-        }
-        $Liste = array();
-        $data = json_decode(file_get_contents(__DIR__ . '/form.json'), true);
-        $InstanceIDListe = IPS_GetInstanceListByModuleID('{1349F095-4820-4DB8-82EB-C1E93E680F08}');
-        foreach ($InstanceIDListe as $InstanceID) {
-            // Fremde Geräte überspringen
-            $TasmotaTopic = IPS_GetProperty($InstanceID, 'Topic');
-            $Tasmotas = array(
-                'InstanceID'   => $InstanceID,
-                'TasmotaType'  => 'Tasmota',
-                'TasmotaTopic' => $TasmotaTopic,
-                'Name'         => IPS_GetName($InstanceID)
-            );
-            $FoundIndex = array_key_exists($TasmotaTopic, $DevicesTopics);
-            if ($FoundIndex === false) {
-                $TasmotaDevices['rowColor'] = '#ff0000';
-            } else {
-                $TasmotaDevices['rowColor'] = '#00ff00';
-                unset($DevicesTopics[$TasmotaTopic]);
+        $data = json_decode(file_get_contents(__DIR__ . '/form.json'));
+        $TasmotaDevices = $this->getTasmotaDevices();
+        if (count($TasmotaDevices) > 0) {
+            foreach ($TasmotaDevices as $device) {
+                $InstanzName = '-';
+                $instanceID = $this->searchTasmotaDevice($device['Topic']);
+                if ($instanceID <> 0) {
+                    $InstanzName = IPS_GetInstance($instanceID)['ModuleInfo']['ModuleName'];
+                }
+                $data->actions[0]->values[] = [
+                    'IP'    => $device['IP'],
+                    'Topic'       => $device['Topic'],
+                    'Module'       => $device['Module'],
+                    'Firmware'       => $device['FW'],
+                    'Instanz'       => $InstanzName,
+                    'instanceID' => $instanceID,
+                    'create'     => [
+                        'Tasmota'     => [
+                        'moduleID'      => '{1349F095-4820-4DB8-82EB-C1E93E680F08}',
+                        'configuration' => [
+                            'Topic'    => $device['Topic']
+                        ]
+                    ],
+                        'TasmotaLED'     => [
+                        'moduleID'      => '{5466CCED-1DA1-4FD9-9CBD-18E9399EFF42}',
+                        'configuration' => [
+                            'Topic'    => $device['Topic']
+                        ]
+                    ],
+                        'TasmotaSwitchTopic'     => [
+                        'moduleID'      => '{74BEB8D0-6BA8-4159-B7B8-E95EB7B29779}',
+                        'configuration' => [
+                            'Topic'    => $device['Topic']
+                        ]
+                    ]
+                    ]
+                ];
             }
-            $Liste[] = $Tasmotas;
         }
-        //Prüfe auf LED Module
-        $InstanceIDListe = IPS_GetInstanceListByModuleID('{5466CCED-1DA1-4FD9-9CBD-18E9399EFF42}');
-        foreach ($InstanceIDListe as $InstanceID) {
-            // Fremde Geräte überspringen
-            $TasmotaTopic = IPS_GetProperty($InstanceID, 'Topic');
-            $Tasmotas = array(
-                'InstanceID'   => $InstanceID,
-                'TasmotaType'  => 'TasmotaLED',
-                'TasmotaTopic' => $TasmotaTopic,
-                'Name'         => IPS_GetName($InstanceID)
-            );
-            $FoundIndex = array_key_exists($TasmotaTopic, $DevicesTopics);
-            if ($FoundIndex === false) {
-                $TasmotaDevices['rowColor'] = '#ff0000';
-            } else {
-                $TasmotaDevices['rowColor'] = '#00ff00';
-                unset($DevicesTopics[$TasmotaTopic]);
-            }
-            $Liste[] = $Tasmotas;
-        }
-        foreach ($DevicesTopics as $key => $Device) {
-            $this->SendDebug('Device', $Device, 0);
-            $Tasmotas = array(
-                'InstanceID'   => 0,
-                'TasmotaType'  => $this->Translate('unknown'),
-                'TasmotaTopic' => $Device,
-                'Name'         => ''
-            );
-            $Liste[] = $Tasmotas;
-        }
-        $data['actions'][2]['values'] = array_merge($data['actions'][2]['values'], $Liste);
-        $data['actions'][4]['onClick'] = '
-        if (($TasmotaDevices["TasmotaTopic"] == "") or ($TasmotaDevices["InstanceID"] > 0))
-            {
-            echo "' . $this->Translate('No tasmota device selected!') . '";
-            return;
-            }
-            
-        switch ($DeviceType) {
-            case 0:
-                $InstanceID = IPS_CreateInstance("{1349F095-4820-4DB8-82EB-C1E93E680F08}");
-                break;
-            case 1:
-                $InstanceID = IPS_CreateInstance("{5466CCED-1DA1-4FD9-9CBD-18E9399EFF42}");
-                break;
-            default:
-                echo "' . $this->Translate('Error on create instance.') . '";
-                return;
-                break;
-        }
-            
-        //$InstanceID = IPS_CreateInstance("{1349F095-4820-4DB8-82EB-C1E93E680F08}");
-        if ($InstanceID == false) return;
-        $ParentID = IPS_GetInstance($InstanceID)["ConnectionID"];
-        if ($ParentID == 0)
-            {
-                echo "' . $this->Translate('Error on create instance.') . '";
-                return;
-            }
-        IPS_SetProperty($InstanceID, "Topic", $TasmotaDevices["TasmotaTopic"]);
-        IPS_SetProperty($InstanceID, "FullTopic", IPS_GetProperty($id,"FullTopic"));
-    
-        @IPS_ApplyChanges($InstanceID);
-        IPS_SetName($InstanceID,"Tasmota - ".$TasmotaDevices["TasmotaTopic"]);
-        echo "OK";
-        ';
-        IPS_LogMessage('Action Button', print_r($data['actions'][3], 1));
+        IPS_LogMessage(__FUNCTION__, json_encode($data));
         return json_encode($data);
     }
 
-    public function ReceiveData($JSONString)
+    private function getTasmotaDevices()
     {
-        $DevicesTopics = unserialize($this->GetBuffer('DevicesTopics'));
-        $this->SendDebug('ReceiveData JSON', $JSONString, 0);
-        $data = json_decode($JSONString);
-        // Buffer decodieren und in eine Variable schreiben
-        //$Buffer = utf8_decode($data->Buffer);
-        $Buffer = $data;
-        $this->SendDebug('ReceiveData JSON', $Buffer->Topic, 0);
-        $TopicArr = explode('/', $Buffer->Topic);
-        $FullTopic = explode('/', $this->ReadPropertyString('FullTopic'));
-        $PrefixIndex = array_search('%prefix%', $FullTopic);
-        $TopicIndex = array_search('%topic%', $FullTopic);
-        $DevicesTopics[$TopicArr[$TopicIndex]] = $TopicArr[$TopicIndex];
-        $this->SetBuffer('DevicesTopics', serialize($DevicesTopics));
-        $this->SendDebug('Count Devices', count($DevicesTopics), 0);
-        $this->SendDebug('Topic', $TopicArr[$TopicIndex], 0);
+        $sIP1 = $this->ReadPropertyString('StartIP');
+        $sIP2 = $this->ReadPropertyString('EndIP');
+        $TasmotaDevices = [];
+
+        if ($sIP1 <> '' && $sIP2 <> '') {
+            $i = 0;
+            if ((ip2long($sIP1) !== -1) && (ip2long($sIP2) !== -1)) // As of PHP5, -1 => False
+            {
+                for ($lIP = ip2long($sIP1) ; $lIP <= ip2long($sIP2) ; $lIP++) {
+                    $result = Sys_Ping($lIP, 30); //Max. 10 ms warten
+                    if ($result) {
+                        $ch = curl_init();
+                        curl_setopt($ch, CURLOPT_URL, long2ip($lIP).'/cm?cmnd=Topic');
+                        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                        curl_setopt($ch, CURLOPT_TIMEOUT, 1);
+                        curl_setopt($ch, CURLOPT_HEADER, false);
+
+                        $apiResultJSON = curl_exec($ch);
+                        $headerInfo = curl_getinfo($ch);
+                        $result = json_decode($apiResultJSON, true);
+                        curl_close($ch);
+                        if (is_array($result)) {
+                            if (array_key_exists('WARNING',$result)) {
+                                $TasmotaDevices[$i]['IP'] = long2ip($lIP);
+                                $TasmotaDevices[$i]['Topic'] ='';
+                                $TasmotaDevices[$i]['Module'] ='';
+                                $TasmotaDevices[$i]['FW'] ='';
+                            }
+                            if (array_key_exists('Topic',$result)) {
+                                $TasmotaDevices[$i]['IP'] = long2ip($lIP);
+                                $TasmotaDevices[$i]['Topic'] = $result['Topic'];
+                                $TasmotaDevices[$i]['Module'] = $this->getModule(long2ip($lIP));
+                                $TasmotaDevices[$i]['FW'] = $this->getFirmwareVersion(long2ip($lIP));
+                            }
+                            $i++;
+                        }
+                    }
+                }
+            }
+        }
+            return $TasmotaDevices;
     }
 
-    public function ScanTasmotaNetwork()
-    {
-        $this->DevicesTopics = array();
-        $Data['DataID'] = '{043EA491-0325-4ADD-8FC2-A30C8EEB4D3F}';
-        $Data['PacketType'] = 3;
-        $Data['QualityOfService'] = 0;
-        $Data['Retain'] = false;
-        $Data['Topic'] = '#';
-        $Data['Payload'] = '';
-        $DataJSON = json_encode($Data);
-        $this->SendDebug(__FUNCTION__, $BufferJSON, 0);
-        $this->SendDataToParent($DataJSON);
+    private function getFirmwareVersion($ip) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $ip.'/cm?cmnd=Status%202');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT_MS, 500);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+
+        $apiResultJSON = curl_exec($ch);
+        $headerInfo = curl_getinfo($ch);
+        $result = json_decode($apiResultJSON,true);
+        curl_close($ch);
+        return $result['StatusFWR']['Version'];
     }
+
+    private function getModule($ip) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $ip.'/cm?cmnd=Module');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT_MS, 500);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+
+        $apiResultJSON = curl_exec($ch);
+        $headerInfo = curl_getinfo($ch);
+        $result = json_decode($apiResultJSON,true);
+        curl_close($ch);
+        return $result['Module'];
+    }
+
+
+
 }
